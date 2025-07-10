4c5a973f9a1336e18ce783f699c5aaa55ede9b18
Winston Wan <winst.wan@gmail.com>
2025-07-09 20:30:21 -0400

    update git blame format

--- lua/custom/plugins/git-blame.lua
+++ lua/custom/plugins/git-blame.lua
@@ -13,7 +13,7 @@ return {
     -- your configuration comes here
     -- for example
     enabled = true, -- if you want to enable the plugin
-    message_template = ' • <author> • <date> • <summary> • <sha>', -- template for the blame message, check the Message template section for more options
+    message_template = ' • <author> • <sha> • <date> • <summary>', -- template for the blame message, check the Message template section for more options
     date_format = '%Y-%m-%d', -- template for the date, check Date format section for more options
     virtual_text_column = 100, -- virtual text start column, check Start virtual text at column section for more options
     delay = 2000,
